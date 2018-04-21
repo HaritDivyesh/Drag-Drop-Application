08b8e66b3efc40784f2771f24a5debba66e2b26d
Divyesh Harit <haritdivyesh@gmail.com>
2018-04-21 10:26:45 -0400

    Basic first+Second name drag drop workspace

--- boxInteractions.js
+++ boxInteractions.js
@@ -1,13 +1,44 @@
 
+// $(function() {
+
+//   $("#workspace").droppable({
+//                 out: function() {
+//                     $( this ).droppable( "option", "disabled", false );
+//                 },
+//                 drop: function( event, ui ) {
+//                     $( this ).droppable( "option", "disabled", true );
+//                 }
+//         });
+
+//   $( "#firstName" ).draggable({
+//     drag: function(event,ui){
+//       if($(this).data('droppedin')){
+//           $(this).data('droppedin').droppable('enable');
+//           $(this).data('droppedin',null);
+//           $(this).removeClass('dropped');
+//         }}
+//   });
+
+// });
+
 $(function() {
-  $( "#draggable" ).draggable();
-  
-  $( "#droppable" ).droppable({
-     drop: function( event, ui ) {
-       $( this )
-         .addClass( "ui-state-highlight" )
-         .find( "p" )
-           .html( "Dropped!" );
-     }
-      });
+        $(".name").draggable({
+                revert: 'invalid',
+                cursor: 'move'
+        });
+    
+        $("#NameBox").droppable({
+                drop: function( event, ui ) {
+                    $("#trash").droppable( "option", "disabled", false );
+                }
+        });
+        
+        $("#WorkSpace").droppable({
+                out: function() {
+                    $( this ).droppable( "option", "disabled", false );
+                },
+                drop: function( event, ui ) {
+                    $( this ).droppable( "option", "disabled", true );
+                }
+        });
 });
